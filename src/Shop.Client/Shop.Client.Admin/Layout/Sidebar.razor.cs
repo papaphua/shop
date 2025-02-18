2ae4eff682aa833e3d3da3d055793f95890503f1
olekssokol <olekssokol@gmail.com>
2025-02-18 12:18:07 +0200

    refactor

--- src/Shop.Client/Shop.Client.Admin/Layout/Sidebar.razor.cs
+++ src/Shop.Client/Shop.Client.Admin/Layout/Sidebar.razor.cs
@@ -6,28 +6,10 @@ public partial class Sidebar
 {
     [Inject] public required NavigationManager NavigationManager { get; set; }
 
-    private string? _currentUri;
-
-    protected override void OnInitialized()
-    {
-        _currentUri = NavigationManager.Uri;
-    }
-
-    protected override void OnAfterRender(bool firstRender)
-    {
-        if (NavigationManager.Uri == _currentUri) return;
-
-        _currentUri = NavigationManager.Uri;
-        StateHasChanged();
-    }
+    private bool _isOnLoginPage => NavigationManager.Uri.EndsWith("/login");
 
     private void NavigateToPage(string page)
     {
         NavigationManager.NavigateTo(page);
-    }
-
-    private bool IsOnLoginPage()
-    {
-        return NavigationManager.Uri.EndsWith("/login");
     }
 }
