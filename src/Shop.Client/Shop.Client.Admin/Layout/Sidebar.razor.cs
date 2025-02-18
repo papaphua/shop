359897ac49d05804902abce7712852bbab01c90e
Yevhenii Nabok <1337niceshot@gmail.com>
2025-02-18 12:37:40 +0200

    fix login button

--- src/Shop.Client/Shop.Client.Admin/Layout/Sidebar.razor.cs
+++ src/Shop.Client/Shop.Client.Admin/Layout/Sidebar.razor.cs
@@ -4,9 +4,18 @@ namespace Shop.Client.Admin.Layout;
 
 public partial class Sidebar
 {
+    private string? _currentUri;
     [Inject] public required NavigationManager NavigationManager { get; set; }
 
     private bool _isOnLoginPage => NavigationManager.Uri.EndsWith("/login");
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (NavigationManager.Uri == _currentUri) return;
+
+        _currentUri = NavigationManager.Uri;
+        StateHasChanged();
+    }
 
     private void NavigateToPage(string page)
     {
