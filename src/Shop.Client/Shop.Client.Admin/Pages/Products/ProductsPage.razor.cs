7524b6393a055914fd6383fb472bc0a177388f38
Yevhenii Nabok <1337niceshot@gmail.com>
2025-02-19 17:25:57 +0200

    fix _isLoading

--- src/Shop.Client/Shop.Client.Admin/Pages/Products/ProductsPage.razor.cs
+++ src/Shop.Client/Shop.Client.Admin/Pages/Products/ProductsPage.razor.cs
@@ -12,38 +12,42 @@ public sealed partial class ProductsPage : ComponentBase
     private PagedList<ProductDto> _list = new();
     private string? _selectedType;
     private List<string> _types = [];
+    private bool _isLoading;
 
     [Inject] public required IProductService ProductService { get; set; }
     [Inject] public required NavigationManager NavigationManager { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+        _isLoading = true;
+        StateHasChanged();
+
         await base.OnInitializedAsync();
 
-        await Task.Delay(5000);
-        
         _types = (await ProductService.GetProductTypesAsync())
             .Select(type => type.Name)
             .ToList();
 
+        _isLoading = false;
+
         await LoadNewPageAsync(1);
     }
 
-    private bool IsLoading()
-    {
-        return _list.Items.Count == 0 && _types.Count == 0;
-    }
-    
     private async Task LoadNewTypeAsync(string? type)
     {
         _selectedType = type;
         await LoadNewPageAsync(1);
     }
-    
+
     private async Task LoadNewPageAsync(int pageNumber)
     {
+        _isLoading = true;
+        StateHasChanged();
+
         var query = new PagingQuery(pageNumber, PageSize);
         _list = await ProductService.GetAsync(query, _selectedType);
+
+        _isLoading = false;
     }
 
     private async Task DeleteProduct(int productId)
